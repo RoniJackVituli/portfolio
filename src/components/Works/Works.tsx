181e1e9e36cb9f7aea09ed93077fd8e58e8ad6c8
RoniJackVituli <rjvtechnology@gmail.com>
2022-09-08 16:29:19 +0300

    fix the slider

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -7,34 +7,21 @@ import "./Works.scss";
 import Title from "../../Layout/Title/Title";
 import projects from "./worksData";
 
-type ProjectData = {
-  title: string;
-  description: string;
-  tech: string;
-  url: string;
-  github: string;
-  img: string;
-};
 
 const Works: React.FC = () => {
-  const [index, setIndex] = useState<number>(1);
+  const [current, setCurrent] = useState<number>(0);
   const length = projects.length;
-  const [project, setProject] = useState<ProjectData>(projects[0]);
-
 
   const moveForward = () => {
-    setIndex((prevIndex) => {
-      return prevIndex === length - 1 ? 0 : prevIndex + 1;
+    setCurrent((prevCurrent) => {
+      return prevCurrent === length - 1 ? 0 : prevCurrent + 1;
     });
-    setProject(projects[index]);
   };
 
   const backForward = () => {
-    setIndex((prevIndex) => {
-      return prevIndex === 0 ? projects.length - 1 : prevIndex - 1;
+    setCurrent((prevCurrent) => {
+      return prevCurrent === 0 ? projects.length - 1 : prevCurrent - 1;
     });
-
-    setProject(projects[index]);
   };
 
   return (
@@ -43,71 +30,77 @@ const Works: React.FC = () => {
         <AnimateOnScroll animation={"fadeIn"}>
           <Title text="works" />
         </AnimateOnScroll>
-
-        <div
-          className="works__background"
-          style={{
-            backgroundImage: `url(${project.img})`,
-            backgroundPosition: "top",
-            backgroundSize: "cover",
-            height: "90vh",
-          }}
-        ></div>
-
-        <div className="works__contact">
-          {/* Arrow Left */}
-          <svg
-            onClick={backForward}
-            width="76"
-            height="76"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="1.5"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            className="left__arrow"
-          >
-            <path d="M11.438 18.75 4.688 12l6.75-6.75"></path>
-            <path d="M5.625 12h13.688"></path>
-          </svg>
-          {/* Arrow Right */}
-          <svg
-            onClick={moveForward}
-            width="76"
-            height="76"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="1.5"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            className="right__arrow"
-          >
-            <path d="m12.563 5.25 6.75 6.75-6.75 6.75"></path>
-            <path d="M18.375 12H4.687"></path>
-          </svg>
-          <div className="work__card box-floating">
-            <div className="card__img">
-              <img src={project.img} alt="lol" />
+        {projects.map((project, index) => {
+          return (
+            <div>
+            {index === current && (<>
+                <div
+                  className="works__background"
+                  style={{
+                    backgroundImage: `url(${project.img})`,
+                    backgroundPosition: "top",
+                    backgroundSize: "cover",
+                    height: "90vh",
+                  }}
+                ></div>
+                <div className="works__contact">
+                  {/* Arrow Left */}
+                  <svg
+                    onClick={backForward}
+                    width="76"
+                    height="76"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="left__arrow"
+                  >
+                    <path d="M11.438 18.75 4.688 12l6.75-6.75"></path>
+                    <path d="M5.625 12h13.688"></path>
+                  </svg>
+                  {/* Arrow Right */}
+                  <svg
+                    onClick={moveForward}
+                    width="76"
+                    height="76"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="right__arrow"
+                  >
+                    <path d="m12.563 5.25 6.75 6.75-6.75 6.75"></path>
+                    <path d="M18.375 12H4.687"></path>
+                  </svg>
+                  <div className="work__card box-floating">
+                    <div className="card__img">
+                      <img src={project.img} alt="lol" />
+                    </div>
+                    <div className="card__description">
+                      <h4>{project.title}</h4>
+                      <p>{project.description}</p>
+                      <div className="card__btn">
+                        <span className="tech">{project.tech}</span>
+                        <a className="btn" href={`${project.url}`}>
+                          Visit <AiOutlineEnter />
+                        </a>
+                        <a className="git" href={`${project.github}`}>
+                          <SiGithub />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>)}
             </div>
-            <div className="card__description">
-              <h4>{project.title}</h4>
-              <p>{project.description}</p>
-              <div className="card__btn">
-                <span className="tech">{project.tech}</span>
-                <a className="btn" href={`${project.url}`}>
-                  Visit <AiOutlineEnter />
-                </a>
-                <a className="git" href={`${project.github}`}>
-                  <SiGithub />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </ScrollContainer>
   );
