683702d2d1880c00272fb868cd988b1d454db237
RoniJackVituli <rjvtechnology@gmail.com>
2022-10-31 17:30:45 +0200

    change projects

--- src/components/Works/worksData.ts
+++ src/components/Works/worksData.ts
@@ -1,17 +1,26 @@
 import estudy from "../../img/estudy.jpeg";
-import gtw from '../../img/gtw.jpeg'
-import ls from '../../img/link-short.jpeg'
-import weather from '../../img/weather.png';
+import gtw from "../../img/gtw.jpeg";
+import ls from "../../img/link-short.jpeg";
+import weather from "../../img/weather.png";
+import LetsDeal from "../../img/lets-deal.png";
 export type ProjectData = {
   title: string;
   description: string;
   tech: string;
   url: string;
-  github: string;
+  github?: string;
   img: string;
 };
 
 const projects: ProjectData[] = [
+  {
+    title: "LetsDeal",
+    description:
+      " The LetsDeal was created in 2022. The website was created for customers who want to find the cheapest price for the product they are looking for. In addition, the website was established in order to create a revolution in the field of deals, coupons, and online sales.\n\nI did not do this project alone, my part of the project was client side.(Frontend)",
+    tech: "React",
+    url: "https://lets-deal.net/",
+    img: LetsDeal,
+  },
   {
     title: "estudy",
     description:
@@ -23,7 +32,8 @@ const projects: ProjectData[] = [
   },
   {
     title: "guess-the-word",
-    description: "Guess The Word is based on the game `'Wordle'` the most popular game today. The technologies I used to develop the game are: React, TypeScript, VScode.",
+    description:
+      "Guess The Word is based on the game `'Wordle'` the most popular game today. The technologies I used to develop the game are: React, TypeScript, VScode.",
     tech: "React",
     url: "https://guess-the-word-rjv.netlify.app/",
     github: "https://github.com/RoniJack/guess-the-word",
@@ -31,24 +41,13 @@ const projects: ProjectData[] = [
   },
   {
     title: "short-url",
-    description: "In this project, I learn how to build a website that shortens URLs. Optimizing the length of your website's URLs is useful on space-sensitive forums, helps websites rank better in search results, and are also easier for users to type out.",
+    description:
+      "In this project, I learn how to build a website that shortens URLs. Optimizing the length of your website's URLs is useful on space-sensitive forums, helps websites rank better in search results, and are also easier for users to type out.",
     tech: "React",
     url: "https://short-url-rjv.herokuapp.com/",
     github: "https://github.com/RoniJack/short-url",
     img: ls,
   },
-  {
-    title: "weather",
-    description: "In this project, I learn how to build a website that show to you the weather in your location. also you can search city that you want to see the weather",
-    tech: "React",
-    url: "https://weather-rjv.netlify.app/",
-    github: "https://github.com/RoniJack/weather-app/tree/main",
-    img: weather,
-  },
-
-
-
-
 ];
 
 export default projects;
